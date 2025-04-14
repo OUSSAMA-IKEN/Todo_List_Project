4b958616d1c4a184ef035802ccbaa5a00f5c4ea5
OUSSAMA-7w <ikenossama913@gmail.com>
2025-04-14 19:41:05 +0100

    palette

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ const theme1 = createTheme({
     MuiCard: {
       styleOverrides: {
         root: {
-          boxShadow: "0 0 30px 2px rgba(102, 179, 255, 0.5)"
+          boxShadow: "0 0 30px 2px rgba(248, 31, 31, 0.66)"
         }
       }
     }
